fd840638f371b52cfbce89e9d920c54f5d81fe7f
Gus Ruben <gusruben@protonmail.com>
2023-01-04 15:18:07 -0500

    NPM files & index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+class Aspen {
+    
+    /**
+     * Creates the Aspen session object, and logs in using the supplied username/password
+     * @param {Object} options The session options
+     * @param {String} options.username The username to use to sign in to Aspen
+     * @param {String} options.password The password to use to sign in to Aspen
+     * @param {String} options.id The ID of the Aspen instance to access, in {id}.myfollett.com
+     */
+    constructor(options) {
+        
+    }
+}
+
+module.exports = Aspen

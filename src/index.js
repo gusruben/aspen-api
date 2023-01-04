6b52970224852ecd9457729b330ae12ed90cfed4
Gus Ruben <gusruben@protonmail.com>
2023-01-04 16:49:27 -0500

    Add constructor The constructor grabs base cookies and the Apache struts token, which is needed to log in.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-
-
+import axios from "axios";
+import { JSDOM } from "jsdom";
 
 class Aspen {
-    
+    rootURL;
+    cookies = {};
+    strutsToken; // apache struts token (org.apache.struts.taglib.html.TOKEN)
+
     /**
      * Creates the Aspen session object, and logs in using the supplied username/password
      * @param {Object} options The session options
@@ -11,8 +14,29 @@ class Aspen {
      * @param {String} options.id The ID of the Aspen instance to access, in {id}.myfollett.com
      */
     constructor(options) {
-        
+        // set base url
+        this.rootURL = `https://${options.id}.myfollett.com`;
+
+        // initial request to create a new JSESSIONID cookie, which is needed
+        // for the rest of the requests (not part of the login, though), and to
+        // get the Apache Struts HTML token (something else it uses to log in)
+        axios.get(this.rootURL).then((resp) => {
+            // extract JSESSIONID cookie (as well as non-necessary cookies)
+            resp.headers["set-cookie"].forEach((cookie) => {
+                // split at first equal sign
+                let cookieSplit = cookie.split(/=(.*)/s);
+                // strip everything after first ';'
+                let cookieData = cookieSplit[1].split(";")[0];
+                this.cookies[cookieSplit[0]] = cookieData;
+            });
+
+            // create dom object to extract apache struts token from form
+            const dom = new JSDOM(resp.data);
+            this.strutsToken = dom.window.document.querySelector(
+                "[name='org.apache.struts.taglib.html.TOKEN']"
+            ).value;
+        });
     }
 }
 
-module.exports = Aspen
+export default Aspen;
